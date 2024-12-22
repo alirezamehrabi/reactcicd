d689a0de97e78e6c798b96490dab772cb1e05584
Alireza <mehrabia95@yahoo.com>
2024-12-22 10:49:45 +0330

    change packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h6>test for gh-pages</h6>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
