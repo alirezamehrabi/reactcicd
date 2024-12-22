66c44c3c1980c397697608a0590265eb193ca36d
Alireza <mehrabia95@yahoo.com>
2024-12-22 11:14:23 +0330

    file changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h6>test for gh-pages</h6>
+        <h6>second change for confirm</h6>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
